4956253077f11c15089c326b3573a17b09f82992
progeuler <sarufkhan568@gmail.com>
2025-05-28 14:18:32 +0600

    Add root endpoint to confirm server is ready

--- src/app.js
+++ src/app.js
@@ -12,4 +12,8 @@ app.use(cors());
 app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('server is ready to serve.')
+});
+
 module.exports = app;
